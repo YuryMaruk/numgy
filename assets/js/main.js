e6b7080cc0a454eff98beeb64c011131f3de35c4
Yury Maruk <maruk.dev@gmail.com>
2023-05-25 21:01:56 +0300

    feat: finish 4 numbers and matrix

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,13 @@ const family = document.querySelector(".family");
 const dateBirthday = document.querySelector(".date-birthday");
 const btnCount = document.querySelector(".button-count");
 const secWorkNum = document.querySelector(".second-working-number");
+const columnsList = document.querySelectorAll('.column');
 
 btnCount.addEventListener('click', handler);
 
 let firstNum, secondNum, thirdNum, fourthNum;
 
-
+console.log(columnsList)
 
 
 function handler() {
@@ -19,11 +20,10 @@ function handler() {
   fourthNum = createSecondNum(thirdNum);
   arrNums = [firstNum, secondNum, thirdNum, fourthNum];
   showSecWorkNum(arrNums, secWorkNum);
-
-}
-
-function addSecNum(one, two, three, four, elem) {
-  elem.value = '${one}.${two}.${three}.${four}';
+  arrMatrix = arrNums.concat(dateBirthday.value.split('-').join(''));
+  stringMatrix = arrMatrix.join('');
+  arr = stringMatrix.split('');
+  parserNodeList(columnsList, arr);
 }
 
 function createFirstNum(dateValue) {
@@ -34,14 +34,21 @@ function createSecondNum(num) {
   return num > 12 ? String(num).split('').reduce((acc, item) => acc + Number(item), 0) : num;
 }
 
-function createThirdNum(dateValue, num){
+function createThirdNum(dateValue, num) {
   arr = dateValue.split('-').join('').split('').reverse();
   result = arr[1] > 0 ? arr[1] : arr[0];
   return num - (2 * result);
 }
 
-function showSecWorkNum (arr, elem){
+function showSecWorkNum(arr, elem) {
   elem.value = arr.join('.');
+}
+
+function parserNodeList(list, arr) {
+  list.forEach(element => {
+    resultArray = arr.filter(item => item === element.id);
+    element.innerText = resultArray.join('');
+  });
 }
 
 
