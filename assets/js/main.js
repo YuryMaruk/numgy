0d2a967c33a8ccbd5575ca922f20362da35c1081
Yury Maruk <maruk.dev@gmail.com>
2023-05-25 16:06:21 +0300

    feat: add funcs which count number

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+const firstName = document.querySelector(".first-name");
+const secondName = document.querySelector(".second-name");
+const family = document.querySelector(".family");
+const dateBirthday = document.querySelector(".date-birthday");
+const btnCount = document.querySelector(".button-count");
+const secWorkNum = document.querySelector(".second-working-number");
+
+btnCount.addEventListener('click', handler);
+
+let firstNum, secondNum, thirdNum, fourthNum;
+
+
+
+
+function handler() {
+  firstNum = createFirstNum(dateBirthday.value);
+  secondNum = createSecondNum(firstNum);
+  thirdNum = createThirdNum(dateBirthday.value);
+  fourthNum = createSecondNum(thirdNum);
+  arrNums = [firstNum, secondNum, thirdNum, fourthNum];
+  showSecWorkNum(arrNums, secWorkNum);
+
+}
+
+function addSecNum(one, two, three, four, elem) {
+  elem.value = '${one}.${two}.${three}.${four}';
+}
+
+function createFirstNum(dateValue) {
+  return dateValue.split('-').join('').split('').reduce((acc, item) => acc + Number(item), 0);
+}
+
+function createSecondNum(num) {
+  return num > 12 ? String(num).split('').reduce((acc, item) => acc + Number(item), 0) : num;
+}
+
+function createThirdNum(dateValue){
+  arr = dateValue.split('-').join('').split('').reverse();
+  result = arr[1] > 0 ? arr[1] : arr[0];
+  return 35 - (2 * result);
+}
+
+function showSecWorkNum (arr, elem){
+  elem.value = arr.join('.');
+}
+
+
+
